86ed0831d74919c99a6c92797cf6a7a014f1b3d3
poorani11 <poorani.mgr@gmail.com>
2015-12-02 18:23:08 +0530

    Added ustom directives to the app

---
+++ controllers.js
@@ -0,0 +1,28 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope','cityService', function($scope, cityService){
+
+    $scope.city = cityService.city;
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+
+    });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource','$routeParams', 'cityService', function($scope, $resource,$routeParams, cityService){
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        callback: "JSON_CALLBACK"},{ get: { method: "JSONP" }});
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid: '2de143494c0b295cca9337e1e96b00e0' });
+     
+    $scope.convertToFarenheit = function(degK){
+        return   Math.round((1.8 * (degK - 273)) + 32);
+    };
+
+    $scope.convertToDate = function(dt){
+        return new Date(dt*1000);
+    };
+
+}]);
